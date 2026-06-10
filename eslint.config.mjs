a3f1b72edbaf545510eceeaa10ce90dae898fecd
Eliel MENGUE <156508960+elielMengue@users.noreply.github.com>
2026-06-10 13:36:21 +0200

    chore: lint Node scripts with proper globals and format fixtures script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default tseslint.config(
   {
@@ -9,6 +10,12 @@ export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
+  {
+    files: ['**/*.mjs'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   {
     rules: {
       '@typescript-eslint/no-unused-vars': [
--- scripts/fetch-fixtures.mjs
+++ scripts/fetch-fixtures.mjs
@@ -4,7 +4,10 @@ import { resolve } from 'node:path';
 const FIXTURES = [
   ['wikipedia-fr-lecture', 'https://fr.wikipedia.org/wiki/Lecture'],
   ['wikipedia-en-reading', 'https://en.wikipedia.org/wiki/Reading'],
-  ['mdn-intersection-observer', 'https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API'],
+  [
+    'mdn-intersection-observer',
+    'https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API',
+  ],
   ['web-dev-cls', 'https://web.dev/articles/cls'],
   ['chrome-dev-mv3', 'https://developer.chrome.com/docs/extensions/develop/migrate/what-is-mv3'],
   ['martinfowler-microservices', 'https://martinfowler.com/articles/microservices.html'],
@@ -12,13 +15,19 @@ const FIXTURES = [
   ['npr-text', 'https://text.npr.org/'],
   ['lemonde-pixels', 'https://www.lemonde.fr/pixels/'],
   ['francetvinfo', 'https://www.francetvinfo.fr/'],
-  ['ars-technica', 'https://arstechnica.com/gadgets/2024/01/what-i-learned-from-using-a-raspberry-pi-5-as-my-main-computer-for-two-weeks/'],
+  [
+    'ars-technica',
+    'https://arstechnica.com/gadgets/2024/01/what-i-learned-from-using-a-raspberry-pi-5-as-my-main-computer-for-two-weeks/',
+  ],
   ['effective-go', 'https://go.dev/doc/effective_go'],
   ['python-tutorial', 'https://docs.python.org/3/tutorial/introduction.html'],
   ['daringfireball-markdown', 'https://daringfireball.net/projects/markdown/syntax'],
   ['paulgraham-essay', 'https://paulgraham.com/words.html'],
   ['overreacted', 'https://overreacted.io/the-two-reacts/'],
-  ['joelonsoftware', 'https://www.joelonsoftware.com/2000/04/06/things-you-should-never-do-part-i/'],
+  [
+    'joelonsoftware',
+    'https://www.joelonsoftware.com/2000/04/06/things-you-should-never-do-part-i/',
+  ],
   ['korben-blog', 'https://korben.info/'],
   ['react-dev-learn', 'https://react.dev/learn/thinking-in-react'],
   ['nodejs-blog', 'https://nodejs.org/en/blog/announcements/v22-release-announce'],
